563aee3a8ec6f6a5f7d4d3b7b43c67a16eaa139b
budsz <4977494+budsz@users.noreply.github.com>
2026-08-07 22:11:42 +0700

    Fixed command cron talk to desktop session

--- battery-notif.sh
+++ battery-notif.sh
@@ -2,6 +2,9 @@
 
 BATPER="$(cat /sys/class/power_supply/BAT0/capacity)"
 
-if [ "$BATPER" -le 43 ]; then
-    notify-send "BATTERY LOW - Please Connect to Charger!"
+if [ "$BATPER" -le 20 ]; then
+    export DISPLAY=:0
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
+    /usr/bin/notify-send -u critical "BATTERY LOW" "Please Connect to Charger! ($BATPER%)"
 fi
