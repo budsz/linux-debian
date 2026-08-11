0da2480a57d901e0030c421e73ac9529d4b92105
budsz <4977494+budsz@users.noreply.github.com>
2026-08-11 21:26:08 +0700

    Fixed logic

--- battery-notif.sh
+++ battery-notif.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/dash
 
 BATPER="$(cat /sys/class/power_supply/BAT0/capacity)"
+BATSTA="$(cat /sys/class/power_supply/BAT0/status)"
 
-if [ "$BATPER" -le 20 ]; then
+if [ "$BATPER" -le 20 ] && [ "$BATSTA" = "Discharging" ]; then
     export DISPLAY=:0
     export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-    /usr/bin/notify-send -u critical "BATTERY LOW" "Please Connect to Charger! ($BATPER%)"
+    /usr/bin/notify-send -u critical "BATTERY LOW" "Please Connect to Charger! ($BATPER% - $BATSTA)"
 fi
