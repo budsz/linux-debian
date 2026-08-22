321d47d140b426007e1a6c6d0ceb89803d1dd4c2
budsz <4977494+budsz@users.noreply.github.com>
2026-08-22 18:53:44 +0700

    Added for `[YT]` category

---
+++ yt-mp4-render.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/dash
+# IT Support & Development.
+# Copyright (c) 2026, Studio Family Karaoke.
+# All rights reserved.
+#
+
+WORKDIR="M-ONE"
+BASEDIR="/home/`whoami`/$WORKDIR"
+FINSDIR="$BASEDIR/Finished"
+LOGODIR="$BASEDIR/logos"
+LOGONAME="yt"
+FFOPT="-hide_banner -nostdin -y"
+
+# Move to basedir.
+if [ ! -d "$BASEDIR" ]; then
+    mkdir $BASEDIR
+    cd $BASEDIR
+else
+    cd $BASEDIR
+fi
+
+# Check logo directory.
+if [ ! -d "$LOGODIR" ]; then
+    mkdir $LOGODIR
+fi
+
+# Built list base on audio files.
+FL="$(find * -type f -name "d-aud-*")"
+if [ -z "$FL" ]; then
+    echo "Audio files doesn't exists!"
+    return 1
+fi
+
+# Main process.
+echo "${FL}" | while read -r dafiles
+do
+    ## Get layout SINGER - TITLE format from audio files.
+    dfsfiles="$(echo $dafiles | awk -F ' - ' '{print $1, "-", $2}' | sed 's/\(.*\)d-aud-//;s/\(.*\)f-aud-//')"
+    dftfiles="$(echo $dafiles | cut -d '-' -f 3,4,5 | sed 's/\.[^.]*$//')"
+    dfofiles="$(echo $dftfiles - ST)"
+
+    ## Build list audio/video files files.
+    dfafiles="$(find * -type f -name "d-aud*$dfsfiles*" \! -iname "*_(Instrumental)*")"
+    dfvfiles="$(find * -type f -name "d-vid*$dfsfiles*")"
+
+    ## Random logo files.
+    ranlogos="$(find $LOGODIR -maxdepth 1 -type f -name "$LOGONAME-*.svg" | shuf -n 1)"
+
+    ## Rendering audio + video + logo.
+    #if [ -n "$dfafiles" ] && [ -n "$dfvfiles" ] && [ ! -f $FINSDIR/"$dfofiles".mpg ]; then
+    if [ -n "$dfafiles" ] && [ -n "$dfvfiles" ]; then
+        ## Checking width video files.
+        wvfiles="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=p=0 -i "$dfvfiles")"
+
+        if [ "$wvfiles" -ge 1280 ]; then
+            ffmpeg $FFOPT -i "$dfafiles" -i "$dfafiles" -i "$dfvfiles" -i "$ranlogos" \
+                -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
+                                 [1:a]aformat=channel_layouts=stereo[a1]; \
+                                 [a0][a1]amerge=inputs=2, \
+                                 pan=stereo|c0=c0|c1=c2[audio], \
+                                 [2:v]scale=-2:720[video]; \
+                                 [3:v]scale=50:50[logo]; \
+                                 [video][logo]overlay=x=main_w-overlay_w-35:y=25:format=auto[outv]" \
+            -c:a aac -ar 48000 -b:a 192k -map "[audio]" \
+            -map "[outv]" -c:v libx264 -crf 20 \
+            -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
+            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
+            -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
+            $FINSDIR/"$dfofiles".mp4
+        elif [ "$wvfiles" -ge 854 ] && [ "$wvfiles" -lt 1280 ]; then
+            ffmpeg $FFOPT -i "$dfafiles" -i "$dfafiles" -i "$dfvfiles" -i "$ranlogos" \
+                -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
+                                 [1:a]aformat=channel_layouts=stereo[a1]; \
+                                 [a0][a1]amerge=inputs=2, \
+                                 pan=stereo|c0=c0|c1=c2[audio], \
+                                 [2:v]scale=-2:720[video]; \
+                                 [3:v]scale=45:45[logo]; \
+                                 [video][logo]overlay=x=main_w-overlay_w-35:y=25:format=auto[outv]" \
+            -c:a aac -ar 48000 -b:a 192k -map "[audio]" \
+            -map "[outv]" -c:v libx264 -crf 20 \
+            -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
+            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
+            -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
+            $FINSDIR/"$dfofiles".mp4
+        elif [ "$wvfiles" -lt 854 ]; then
+            ffmpeg $FFOPT -i "$dfafiles" -i "$dfafiles" -i "$dfvfiles" -i "$ranlogos" \
+                -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
+                                 [1:a]aformat=channel_layouts=stereo[a1]; \
+                                 [a0][a1]amerge=inputs=2, \
+                                 pan=stereo|c0=c0|c1=c2[audio], \
+                                 [2:v]scale=-2:720[video]; \
+                                 [3:v]scale=40:40[logo]; \
+                                 [video][logo]overlay=x=main_w-overlay_w-35:y=25:format=auto[outv]" \
+            -c:a aac -ar 48000 -b:a 192k -map "[audio]" \
+            -map "[outv]" -c:v libx264 -crf 20 \
+            -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
+            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
+            -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
+            $FINSDIR/"$dfofiles".mp4
+        fi
+    else
+        echo "$dfsfiles: Audio/video/logo files: NULL -OR- target file already exists."
+    fi
+done
+
+# CD current directory.
+cd
