df6a37f685dc1843e630c8987d3b2e6e42d85195
budsz <4977494+budsz@users.noreply.github.com>
2026-08-24 08:28:23 +0700

    Fixed for char `[...]` matching

--- yt-mp4-render.sh
+++ yt-mp4-render.sh
@@ -25,35 +25,40 @@ if [ ! -d "$LOGODIR" ]; then
 fi
 
 # Built list base on audio files.
-FL="$(find * -type f -name "d-aud-*")"
+FL="$(find * -type f -name "y-aud-*")"
 if [ -z "$FL" ]; then
-    echo "Audio files doesn't exists!"
+    echo "YT Audio files doesn't exists!"
     return 1
 fi
 
 # Main process.
-echo "${FL}" | while read -r dafiles
+echo "${FL}" | while read -r yafiles
 do
+    ## Fixed '[]' to '\[\]' chars.
+    yafiles="$(echo $yafiles | sed 's/\[/\\[/g;s/\]/\\]/g')"
+
     ## Get layout SINGER - TITLE format from audio files.
-    dfsfiles="$(echo $dafiles | awk -F ' - ' '{print $1, "-", $2}' | sed 's/\(.*\)d-aud-//;s/\(.*\)f-aud-//')"
-    dftfiles="$(echo $dafiles | cut -d '-' -f 3,4,5 | sed 's/\.[^.]*$//')"
-    dfofiles="$(echo $dftfiles - ST)"
+    yfsfiles="$(echo $yafiles | awk -F ' - ' '{print $1, "-", $2}' | sed 's/\(.*\)y-aud-//')"
+    yftfiles="$(echo $yafiles | cut -d '-' -f 3,4,5 | sed 's/\.[^.]*$//')"
+
+    ## Fixed '\[\]' to '[]' chars.
+    yfofiles="$(echo $yftfiles - ST | sed 's/\\\[/[/g;s/\\\]/]/g')"
 
     ## Build list audio/video files files.
-    dfafiles="$(find * -type f -name "d-aud*$dfsfiles*" \! -iname "*_(Instrumental)*")"
-    dfvfiles="$(find * -type f -name "d-vid*$dfsfiles*")"
+    yfafiles="$(find . -type f -name "y-aud*$yfsfiles*" \! -iname "*_(Instrumental)*")"
+    yfvfiles="$(find . -type f -name "y-vid*$yfsfiles*")"
 
     ## Random logo files.
     ranlogos="$(find $LOGODIR -maxdepth 1 -type f -name "$LOGONAME-*.svg" | shuf -n 1)"
 
     ## Rendering audio + video + logo.
-    #if [ -n "$dfafiles" ] && [ -n "$dfvfiles" ] && [ ! -f $FINSDIR/"$dfofiles".mpg ]; then
-    if [ -n "$dfafiles" ] && [ -n "$dfvfiles" ]; then
+    #if [ -n "$yfafiles" ] && [ -n "$yfvfiles" ] && [ ! -f $FINSDIR/"$yfofiles".mpg ]; then
+    if [ -n "$yfafiles" ] && [ -n "$yfvfiles" ]; then
         ## Checking width video files.
-        wvfiles="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=p=0 -i "$dfvfiles")"
+        wvfiles="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=p=0 -i "$yfvfiles")"
 
         if [ "$wvfiles" -ge 1280 ]; then
-            ffmpeg $FFOPT -i "$dfafiles" -i "$dfafiles" -i "$dfvfiles" -i "$ranlogos" \
+            ffmpeg $FFOPT -i "$yfafiles" -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
                 -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
                                  [1:a]aformat=channel_layouts=stereo[a1]; \
                                  [a0][a1]amerge=inputs=2, \
@@ -66,9 +71,9 @@ do
             -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
             -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
             -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
-            $FINSDIR/"$dfofiles".mp4
+            $FINSDIR/"$yfofiles".mp4
         elif [ "$wvfiles" -ge 854 ] && [ "$wvfiles" -lt 1280 ]; then
-            ffmpeg $FFOPT -i "$dfafiles" -i "$dfafiles" -i "$dfvfiles" -i "$ranlogos" \
+            ffmpeg $FFOPT -i "$yfafiles" -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
                 -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
                                  [1:a]aformat=channel_layouts=stereo[a1]; \
                                  [a0][a1]amerge=inputs=2, \
@@ -81,9 +86,9 @@ do
             -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
             -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
             -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
-            $FINSDIR/"$dfofiles".mp4
+            $FINSDIR/"$yfofiles".mp4
         elif [ "$wvfiles" -lt 854 ]; then
-            ffmpeg $FFOPT -i "$dfafiles" -i "$dfafiles" -i "$dfvfiles" -i "$ranlogos" \
+            ffmpeg $FFOPT -i "$yfafiles" -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
                 -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
                                  [1:a]aformat=channel_layouts=stereo[a1]; \
                                  [a0][a1]amerge=inputs=2, \
@@ -96,10 +101,10 @@ do
             -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
             -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
             -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
-            $FINSDIR/"$dfofiles".mp4
+            $FINSDIR/"$yfofiles".mp4
         fi
     else
-        echo "$dfsfiles: Audio/video/logo files: NULL -OR- target file already exists."
+        echo "$yfsfiles: Audio/video/logo files: NULL -OR- target file already exists."
     fi
 done
 
