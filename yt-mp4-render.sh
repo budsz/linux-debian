6126e6546b829aef8b165025ca35384dd276e80c
budsz <4977494+budsz@users.noreply.github.com>
2026-08-31 00:04:50 +0700

    Trying to fix logos blurry

--- yt-mp4-render.sh
+++ yt-mp4-render.sh
@@ -55,53 +55,54 @@ do
     #if [ -n "$yfafiles" ] && [ -n "$yfvfiles" ] && [ ! -f $FINSDIR/"$yfofiles".mpg ]; then
     if [ -n "$yfafiles" ] && [ -n "$yfvfiles" ]; then
         ## Checking width video files.
-        wvfiles="$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=p=0 -i "$yfvfiles")"
+        hvfiles="$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=p=0 -i "$yfvfiles")"
 
-        if [ "$wvfiles" -ge 1280 ]; then
-            ffmpeg $FFOPT -i "$yfafiles" -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
-                -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
-                                 [1:a]aformat=channel_layouts=stereo[a1]; \
-                                 [a0][a1]amerge=inputs=2, \
-                                 pan=stereo|c0=c0|c1=c2[audio], \
-                                 [2:v]scale=-2:$wvfiles[video]; \
-                                 [3:v]scale=80:80[logo]; \
-                                 [video][logo]overlay=x=main_w-overlay_w-35:y=25:format=auto[outv]" \
-            -c:a aac -ar 48000 -b:a 192k -map "[audio]" \
+        if [ "$hvfiles" -ge 720 ]; then
+            ffmpeg $FFOPT -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
+                -filter_complex "[1:v]scale=-2:$hvfiles[video]; \
+                                 [2:v]scale=50:50[logo]; \
+                                 [video][logo]overlay=x=main_w-overlay_w-30:y=25:format=auto[outv]" \
+            -c:a aac -ar 48000 -b:a 192k -map 0:a \
             -map "[outv]" -c:v libx264 -crf 20 \
             -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
-            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
+            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-YT" \
             -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
             $FINSDIR/"$yfofiles".mp4
-        elif [ "$wvfiles" -ge 854 ] && [ "$wvfiles" -lt 1280 ]; then
-            ffmpeg $FFOPT -i "$yfafiles" -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
-                -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
-                                 [1:a]aformat=channel_layouts=stereo[a1]; \
-                                 [a0][a1]amerge=inputs=2, \
-                                 pan=stereo|c0=c0|c1=c2[audio], \
-                                 [2:v]scale=-2:$wvfiles[video]; \
-                                 [3:v]scale=60:60[logo]; \
-                                 [video][logo]overlay=x=main_w-overlay_w-35:y=25:format=auto[outv]" \
-            -c:a aac -ar 48000 -b:a 192k -map "[audio]" \
+        elif [ "$hvfiles" -ge 480 ] && [ "$hvfiles" -le 720 ]; then
+            ffmpeg $FFOPT -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
+                -filter_complex "[1:v]scale=-2:$hvfiles[video]; \
+                                 [2:v]scale=40:40[logo]; \
+                                 [video][logo]overlay=x=main_w-overlay_w-25:y=20:format=auto[outv]" \
+            -c:a aac -ar 48000 -b:a 192k -map 0:a \
             -map "[outv]" -c:v libx264 -crf 20 \
             -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
-            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
+            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-YT" \
             -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
             $FINSDIR/"$yfofiles".mp4
-        elif [ "$wvfiles" -lt 854 ]; then
-            ffmpeg $FFOPT -i "$yfafiles" -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
-                -filter_complex "[0:a]aformat=channel_layouts=stereo[a0]; \
-                                 [1:a]aformat=channel_layouts=stereo[a1]; \
-                                 [a0][a1]amerge=inputs=2, \
-                                 pan=stereo|c0=c0|c1=c2[audio], \
-                                 [2:v]scale=-2:$wvfiles[video]; \
-                                 [3:v]scale=40:40[logo]; \
-                                 [video][logo]overlay=x=main_w-overlay_w-35:y=25:format=auto[outv]" \
-            -c:a aac -ar 48000 -b:a 192k -map "[audio]" \
+        elif [ "$hvfiles" -ge 360 ] && [ "$hvfiles" -le 480 ]; then
+            ffmpeg $FFOPT -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
+                -filter_complex "[1:v]scale=-2:$hvfiles[video]; \
+                                 [2:v]scale=30:30[logo]; \
+                                 [video][logo]overlay=x=main_w-overlay_w-20:y=15:format=auto[outv]" \
+            -c:a aac -ar 48000 -b:a 192k -map 0:a \
             -map "[outv]" -c:v libx264 -crf 20 \
             -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
-            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-uar8fps0" \
+            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-YT" \
             -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
             $FINSDIR/"$yfofiles".mp4
+        elif [ "$hvfiles" -ge 240 ] && [ "$hvfiles" -le 360 ]; then
+            ffmpeg $FFOPT -i "$yfafiles" -i "$yfvfiles" -i "$ranlogos" \
+                -filter_complex "[1:v]scale=-2:$hvfiles[video]; \
+                                 [2:v]scale=20:20[logo]; \
+                                 [video][logo]overlay=x=main_w-overlay_w-15:y=10:format=auto[outv]" \
+            -c:a aac -ar 48000 -b:a 192k -map 0:a \
+            -map "[outv]" -c:v libx264 -crf 20 \
+            -metadata:s handler_name="IT & Sound Dept -- Studio Family Karaoke" \
+            -metadata:g encoding_tool="Modified Encoding by eSFK-1.0.5-YT" \
+            -fflags +bitexact -flags:v +bitexact -flags:a +bitexact \
+            $FINSDIR/"$yfofiles".mp4
+        else
+            echo "Unable to determine screen and video resolution!"
         fi
     else
         echo "$yfsfiles: Audio/video/logo files: NULL -OR- target file already exists."
